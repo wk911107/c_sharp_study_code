2eebf3d7fbe881b8f72011a0db31da9c47d92452
wking <466824390@qq.com>
2019-02-14 13:48:49 +0800

    代码提交

---
+++ PipeClientDemo/PipeClientDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace PipeClientDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PipeWriter("TheRocks");
+
+
+        }
+
+        public static void PipeWriter(string pipeName)
+        {
+            var pipeWriter = new NamedPipeClientStream(".", pipeName, PipeDirection.Out);
+            using (var writer = new StreamWriter(pipeWriter))
+            {
+                pipeWriter.Connect();
+                WriteLine("writer connected");
+
+                bool completed = false;
+                while (!completed)
+                {
+                    string input = ReadLine();
+                    if (input == "bye") completed = true;
+
+                    writer.WriteLine(input);
+                    writer.Flush();
+                }
+            }
+            WriteLine("completed writing");
+        }
+
+    }
+}
---
+++ PipeServerDemo/PipeServerDemo/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace PipeServerDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //管道名称，客户端与服务端要一致
+            PipesReader("TheRocks");
+        }
+
+        private static void PipesReader(string pipeName)
+        {
+            try
+            {
+                var pipeReader = new NamedPipeServerStream(pipeName, PipeDirection.In);
+                using (var reader = new StreamReader(pipeReader))
+                {
+                    pipeReader.WaitForConnection();
+                    WriteLine("reader connected");
+
+                    //const int BUFFERSIZE = 256;
+
+                    bool completed = false;
+                    while (!completed)
+                    {
+                        //byte[] buffer = new byte[BUFFERSIZE];
+                        //int nRead = pipeReader.Read(buffer, 0, BUFFERSIZE);
+                        //string line = Encoding.UTF8.GetString(buffer, 0, nRead);
+                        string line = reader.ReadLine();
+                        WriteLine(line);
+                        if (line == "bye") completed = true;
+                    }
+                }
+                WriteLine("completed reading");
+                ReadLine();
+            }
+            catch (Exception ex)
+            {
+                WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
