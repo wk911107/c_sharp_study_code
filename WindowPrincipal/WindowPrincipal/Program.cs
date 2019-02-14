f321f00dd33177b2a87e15413c1f3d22641246e4
wking <466824390@qq.com>
2019-02-14 18:08:58 +0800

    WindowPrincipal(获取当前账户信息)实例

---
+++ WindowPrincipal/WindowPrincipal/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace WindowPrincipal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WindowsIdentity identity = ShowIdentityInformation();
+            //WindowsPrincipal principal;
+
+            Console.ReadKey();
+        }
+
+        public static WindowsIdentity ShowIdentityInformation()
+        {
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            if (identity == null)
+            {
+                WriteLine("not a Windows Identity");
+                return null;
+            }
+
+            WriteLine($"IdentityType: {identity}");
+            WriteLine($"Name: {identity.Name}");
+            WriteLine($"Authenticated: {identity.IsAuthenticated}");
+            WriteLine($"Authentication Type:{identity.AuthenticationType}");
+            WriteLine($"Anonymous? {identity.IsAnonymous}");
+            WriteLine($"Access Token: {identity.AccessToken.DangerousGetHandle()}");
+            WriteLine();
+
+            return identity;
+        }
+
+    }
+}
